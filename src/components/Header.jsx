83e45f4f6fe05c6b54f03def6380f57c61ae3b24
faritdev <albavi.farit2@gmail.com>
2023-11-08 02:48:08 -0300

    sc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,11 +16,12 @@ function Header() {
     }
 
     return (
-        <nav className='bg-[#ede7db] p-5 flex-row-reverse gap-5 lg:flex-row lg:justify-between lg:p-10 '>
-            <div className='flex flex-row-reverse justify-between items-center xl:flex-row'>
+        <nav className='bg-[#ede7db] p-5 flex-row-reverse gap-5 sm:flex-row sm:justify-between lg:p-10 '>
 
-                <h1 className='font-bold text-2xl lg:text-4xl text-center sm:hidden'>LANDING PAGE</h1>
 
+            <div className='flex flex-row-reverse justify-between items-center lg:flex-row'>
+
+                <h1 className='font-bold text-2xl lg:text-4xl text-center lg:hidden'>LANDING PAGE</h1>
                 <button onClick={openMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -30,13 +31,12 @@ function Header() {
 
             </div>
 
-            
-            <div className='lg:flex lg:gap-16 items-center justify-between'>
+
+            <div className='sm:flex lg:gap-16 items-center justify-between'>
 
                 <h1 className='font-bold text-2xl lg:text-4xl text-center hidden lg:block'>LANDING PAGE</h1>
 
-
-                <div className='flex gap-4 items-center sm:'>
+                <div className='flex gap-4 items-center'>
                     <ul className='hidden gap-4 flex-col lg:flex lg:flex-row mt-8 sm:mt-0' id='menu'>
                         <li className='hover:underline hover:underline-offset-8 '><Link to={'/'}>Home</Link></li>
                         <li className='hover:underline hover:underline-offset-8 '><Link to={'/about'}>Quienes somos</Link></li>
