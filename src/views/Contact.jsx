144e205669716322f20ede5b3ce34fdd40866e20
faritdev <albavi.farit2@gmail.com>
2023-11-09 18:29:44 -0300

    sc

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -10,7 +10,7 @@ function Contact() {
             <section className=' max-w-5xl m-auto'>
 
 
-                <aside className='border mx-5'>
+                <aside className='border m-5 p-5'>
                     <div className='flex gap-5 items-center'>
                         <svg class="w-6 h-6 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
                             <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
@@ -19,7 +19,6 @@ function Contact() {
                         <label>
                             gymlgfitness@gmail.com
                         </label>
-
                     </div>
 
                     <div className='flex gap-5 items-center'>
@@ -30,6 +29,7 @@ function Contact() {
                             0992382082
                         </label>
                     </div>
+
                     <div className='flex gap-5 items-center'>
                         <svg class="w-6 h-6 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
                             <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
