1e6f256de9021bd66528273757ccc5fd906c2e7c
faritdev <albavi.farit2@gmail.com>
2023-11-09 19:10:43 -0300

    sc

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -6,38 +6,41 @@ const src = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14403.81145674
 function Contact() {
     return (
         <main className='w-screen text-black overflow-hidden md:pb-20'>
-            
-            <h1 className='text-3xl font-bold text-center my-5 md:my-10'>Contacto</h1>
 
-            <section className='max-w-7xl m-auto md:flex md:gap-5 md:border h-[600px] md:items-center'>
+
+            <section className='max-w-7xl m-auto md:flex md:gap-5 md:border h-[600px] md:items-center mt-10 xl:mt-20'>
 
                 <aside className='m-5 p-5'>
-                    <div className='flex gap-5 items-center'>
-                        <svg class="w-6 h-6 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                            <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
-                            <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
-                        </svg>
-                        <label>
-                            gymlgfitness@gmail.com
-                        </label>
-                    </div>
+                    <h1 className='text-3xl font-bold text-center mb-5 md:mb-10'>Contacto</h1>
+                    <div>
 
-                    <div className='flex gap-5 items-center mt-5'>
-                        <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
-                            <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z" />
-                        </svg>
-                        <label>
-                            0992382082
-                        </label>
-                    </div>
+                        <div className='flex gap-5 items-center'>
+                            <svg class="w-6 h-6 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                                <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                                <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                            </svg>
+                            <label>
+                                gymlgfitness@gmail.com
+                            </label>
+                        </div>
 
-                    <div className='flex gap-5 items-center mt-5'>
-                        <svg class="w-6 h-6 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
-                            <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
-                        </svg>
-                        <label>
-                            Av Mcal. Lopez c/ Av Lomas valentinas, Villeta, Paraguay
-                        </label>
+                        <div className='flex gap-5 items-center mt-5'>
+                            <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
+                                <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z" />
+                            </svg>
+                            <label>
+                                0992382082
+                            </label>
+                        </div>
+
+                        <div className='flex gap-5 items-center mt-5'>
+                            <svg class="w-6 h-6 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
+                            </svg>
+                            <label>
+                                Av Mcal. Lopez c/ Av Lomas valentinas, Villeta, Paraguay
+                            </label>
+                        </div>
                     </div>
                 </aside>
 
