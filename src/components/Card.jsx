2a9c4b00fd595bfc58201cfcb687b56f4e24134e
faritdev <albavi.farit2@gmail.com>
2023-11-09 17:03:34 -0300

    sc

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,10 @@ import React from 'react'
 function Card({ img, name, descript }) {
     return (
 
-        <div class="max-w-sm bg-white  rounded-lg shadow dark:bg-gray-800 ">
-            <img class="" src={img} alt="" />
+        <div class="max-w-[330px] max-h-[600px]  rounded-xl shadow bg-gray-950 ">
+            <img class="rounded-t-lg border" src={img} alt="Personal Trainer Photo" />
             <div class="p-5">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-                </a>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
                 <div>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                         {descript}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,9 @@ import Carousel from '../components/Carousel'
 import { Routes, Route } from 'react-router-dom'
 import About from '../views/About'
 import Card from '../components/Card'
+import person from '../img/person.jpg'
 
+const src = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14403.811456746424!2d-57.5665432!3d-25.5066185!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x945c559aa4443f67%3A0x52f88c1227f63e4c!2sLG%20Fitness%20Gym!5e0!3m2!1ses-419!2spy!4v1699558411546!5m2!1ses-419!2spy'
 
 function Home() {
   const [count, setCount] = useState(0)
@@ -18,24 +20,28 @@ function Home() {
       <article>
       </article>
 
-      <section>
+      <section className='max-w-7xl m-auto'>
         <h1 class="acheuno" className="font-bold text-center text-xl m-6">Instructores</h1>
 
         <article className='flex xl:gap-10 flex-wrap justify-center gap-5 px-5'>
-          <Card img={'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?auto=format&fit=crop&q=80&w=2139&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+          <Card img={person}
             name={'Arnaldo A.'} descript={'Entrena con Arnaldo A. desde las 12:00hs'} />
-          <Card img={'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?auto=format&fit=crop&q=80&w=2139&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
-            name={'violeta Samaniego'} descript={'Entrena con Violeta apartir de las 6:00hs'} />
-          <Card img={'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?auto=format&fit=crop&q=80&w=2139&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+          <Card img={person}
+            name={'Violeta Samaniego'} descript={'Entrena con Violeta apartir de las 6:00hs'} />
+          <Card img={person}
             name={'Sol Cardozo'} descript={'Entrena con Sol apartir de las 21:00hs'} />
 
         </article>
 
       </section>
 
-      <article>
-        <h1 className='text-center p-12 '>UBICACION</h1>
-
+      <article className='lg:p-12'>
+        <h1 className='text-center'>UBICACION</h1>
+        <iframe src={src}
+          className='w-full h-96 sm:h-[340px] sm:max-w-7xl m-auto sm:block sm:p-0'
+          allowFullScreen="" loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade">
+        </iframe>
 
       </article>
 
